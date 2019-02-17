47d7f6fa604bd41d0a1f8888331e01c648e11288
tksato <takayukisatou19801015@gmail.com>
2019-02-17 17:12:41 +0900

    first commit

---
+++ AWS.Wrapper.S3.Sample/AWSS3Sample.cs
@@ -0,0 +1,131 @@
+﻿using Amazon;
+using Amazon.S3;
+using Amazon.S3.Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AWS.Wrapper.S3.Sample
+{
+    /// <summary>
+    /// sample class for aws s3 client
+    /// </summary>
+    public class AWSS3Sample
+    {
+        /// <summary>
+        /// access key id
+        /// </summary>
+        private readonly string _accessKeyId = "";
+
+        /// <summary>
+        /// secret access key
+        /// </summary>
+        private readonly string _secretAccessKey = "";
+
+        /// <summary>
+        /// region
+        /// </summary>
+        private readonly RegionEndpoint _region = RegionEndpoint.APNortheast1;
+
+        /// <summary>
+        /// backet name
+        /// </summary>
+        private readonly string _backetName = "";
+
+        /// <summary>
+        /// expire(minites)
+        /// </summary>
+        private readonly double _expireMinites = 5d;
+
+        #region method
+
+        /// <summary>
+        /// get signed download url
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetSignedDownloadUrl(string key)
+        {
+            using (var client = new AWSS3Client(_accessKeyId, _secretAccessKey, _region))
+            {
+                return client.GetPreSignedUrl(_backetName, key, HttpVerb.GET, _expireMinites);
+            }
+        }
+
+        /// <summary>
+        /// get signed upload url
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public string GetSignedUploadUrl(string key)
+        {
+            using (var client = new AWSS3Client(_accessKeyId, _secretAccessKey, _region))
+            {
+                return client.GetPreSignedUrl(_backetName, key, HttpVerb.PUT, _expireMinites);
+            }
+        }
+
+        /// <summary>
+        /// download object
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="filePath"></param>
+        /// <param name="appendFile"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task DownloadObjectAsync(string key, string filePath, bool appendFile, CancellationToken cancellationToken)
+        {
+            using (var client = new AWSS3Client(_accessKeyId, _secretAccessKey, _region))
+            {
+                using (var result = await client.GetObjectAsync(_backetName, key, _expireMinites))
+                {
+                    await result.WriteResponseStreamToFileAsync(filePath, appendFile, cancellationToken);
+                }
+            }
+        }
+
+        /// <summary>
+        /// download object
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public async Task<PutObjectResponse> UploadObjectAsync(string key, string filePath)
+        {
+            using (var client = new AWSS3Client(_accessKeyId, _secretAccessKey, _region))
+            {
+                 return await client.PutObjectAsync(_backetName, key, filePath);
+            }
+        }
+
+        /// <summary>
+        /// get object keys
+        /// </summary>
+        /// <param name="prefix"></param>
+        /// <returns></returns>
+        public async Task<IEnumerable<string>> GetObjectKeysAsync(string prefix)
+        {
+            var objectKeys = new List<string>();
+
+            using (var client = new AWSS3Client(_accessKeyId, _secretAccessKey, _region))
+            {
+                string continuationToken = null;
+                var isTruncated = true;
+                while (isTruncated)
+                {
+                    var result = await client.ListObjectsV2Async(_backetName, prefix, continuationToken);
+                    if (result.S3Objects != null && result.S3Objects.Any())
+                    {
+                        objectKeys.AddRange(result.S3Objects.Select(s3Object => s3Object.Key).ToArray());
+                    }
+                    isTruncated = result.IsTruncated;
+                    continuationToken = result.NextContinuationToken;
+                }
+            }
+            return objectKeys;
+        }
+
+        #endregion
+    }
+}
---
+++ AWS.Wrapper.S3/AWSS3Client.cs
@@ -0,0 +1,138 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace AWS.Wrapper.S3
+{
+    /// <summary>
+    /// opration class for AWS S3.
+    /// </summary>
+    public class AWSS3Client : IDisposable
+    {
+        /// <summary>
+        /// AmazonS3Client instance
+        /// </summary>
+        private AmazonS3Client _client;
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="accessKeyId"></param>
+        /// <param name="secretAccessKey"></param>
+        /// <param name="regionEndpoint"></param>
+        /// <param name="timeout"></param>
+        public AWSS3Client(string accessKeyId, string secretAccessKey, Amazon.RegionEndpoint regionEndpoint, double timeout = 600d)
+        {
+            var config = new AmazonS3Config
+            {
+                RegionEndpoint = regionEndpoint,
+                Timeout = TimeSpan.FromSeconds(timeout),
+            };
+            _client = new AmazonS3Client(accessKeyId, secretAccessKey, config);
+        }
+
+        /// <summary>
+        /// dispose
+        /// </summary>
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+
+        /// <summary>
+        /// get expire(by DateTime.Now)
+        /// </summary>
+        /// <param name="expireMinites"></param>
+        /// <returns></returns>
+        private DateTime GetExpire(double expireMinites)
+        {
+            return DateTime.Now.AddMinutes(expireMinites);
+        }
+
+        /// <summary>
+        /// get signed url
+        /// </summary>
+        /// <param name="backetName"></param>
+        /// <param name="key"></param>
+        /// <param name="verb"></param>
+        /// <param name="expireMinites"></param>
+        /// <returns></returns>
+        public string GetPreSignedUrl(string backetName, string key, HttpVerb verb, double expireMinites)
+        {
+            var request = new GetPreSignedUrlRequest
+            {
+                BucketName = backetName,
+                Verb = verb,
+                Key = key,
+                Expires = GetExpire(expireMinites),
+            };
+
+            return _client.GetPreSignedURL(request);
+        }
+
+        /// <summary>
+        /// get objects by async
+        /// </summary>
+        /// <param name="backetName"></param>
+        /// <param name="key"></param>
+        /// <param name="expireMinites"></param>
+        /// <returns></returns>
+        public async Task<GetObjectResponse> GetObjectAsync(string backetName, string key, double expireMinites)
+        {
+            var request = new GetObjectRequest
+            {
+                BucketName = backetName,
+                Key = key,
+                ResponseExpiresUtc = GetExpire(expireMinites),
+            };
+
+            return await _client.GetObjectAsync(request);
+        }
+
+        /// <summary>
+        /// put objects by async
+        /// </summary>
+        /// <param name="backetName"></param>
+        /// <param name="key"></param>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        public async Task<PutObjectResponse> PutObjectAsync(string backetName, string key, string filePath)
+        {
+            var request = new PutObjectRequest
+            {
+                BucketName = backetName,
+                Key = key,
+                FilePath = filePath,
+            };
+            return await _client.PutObjectAsync(request);
+        }
+
+        private ListObjectsV2Request _listObjectsV2Request { get; set; }
+        private ListObjectsV2Request ListObjectsV2Request
+        {
+            get
+            {
+                _listObjectsV2Request = _listObjectsV2Request ?? new ListObjectsV2Request();
+                return _listObjectsV2Request;
+            }
+        }
+
+        /// <summary>
+        /// listObjects by by async
+        /// </summary>
+        /// <param name="backetName"></param>
+        /// <param name="prefix"></param>
+        /// <param name="continuationToken"></param>
+        /// <returns></returns>
+        public async Task<ListObjectsV2Response> ListObjectsV2Async(string backetName, string prefix, string continuationToken = null)
+        {
+
+            ListObjectsV2Request.BucketName = backetName;
+            ListObjectsV2Request.Prefix = prefix;
+            ListObjectsV2Request.ContinuationToken = continuationToken;
+
+            return await _client.ListObjectsV2Async(ListObjectsV2Request);
+        }
+    }
+}
